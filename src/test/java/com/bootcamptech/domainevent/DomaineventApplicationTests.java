ecb30e6e0611defdf90e925cce70a5eec4e3a4ba
Alok Kumar Ailawadi <alok99.misc@gmail.com>
2019-04-07 21:24:31 +0530

    first commit

---
+++ src/test/java/com/bootcamptech/domainevent/DomaineventApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bootcamptech.domainevent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DomaineventApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
